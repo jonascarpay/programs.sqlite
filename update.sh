3bfcf8348c13e20c9b3dc819b76ac07e1270b781
Jonas Carpay <jonascarpay@gmail.com>
2023-06-10 12:19:00 +0900

    Initial commit

---
+++ update.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+cleanup() {
+	rm -rf tmp.tar.xz
+	rm -rf tmp
+}
+
+trap cleanup EXIT
+
+declare -a channels=(
+	"nixos-unstable"
+	"nixos-23.05"
+)
+
+for channel in ${channels[@]}; do
+	echo "Updating $channel.."
+
+	REV_URL="https://channels.nixos.org/$channel/git-revision"
+	TAR_URL="https://channels.nixos.org/$channel/nixexprs.tar.xz"
+
+	rm -rf $channel/
+	mkdir -p $channel/
+
+	curl -fLo $channel/git-revision "$REV_URL"
+
+	curl -fLo tmp.tar.xz "$TAR_URL"
+	mkdir -p tmp
+	tar xf tmp.tar.xz --directory=tmp
+	mv $(find tmp -name 'programs.sqlite') $channel/
+
+	cleanup
+
+done
